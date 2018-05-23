c15aed7307704870ef0027b55cbd7330a051dbd9
Sasa Prsir <s.prsir@thepimcompany.com>
2018-05-23 15:32:47 +0200

    Test see the title.

---
+++ forum/tests/Feature/ThreadsTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_user_can_browse_threads()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads');
+
+        $response->assertSee($thread->title);
+    }
+}
